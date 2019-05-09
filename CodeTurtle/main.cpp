7cbb1d9c6b83843a0a45bd82c6a0de7ada4ace59
Mauricio <wmedinap@ulasalle.edu.pe>
2019-05-09 16:15:53 -0500

    Main empty

--- CodeTurtle/main.cpp
+++ CodeTurtle/main.cpp
@@ -10,25 +10,6 @@ int main(int argc, char **argv){
     Turtle T(700,500);
     T.display(argc,argv);
     T.move(150,200);
-    T.forward(75,60);
-    T.forward(75,60);
-    T.forward(75,240);
-    T.forward(75,60);
-    T.forward(75,240);
-    T.forward(75,60);
-    T.forward(75,240);
-    T.forward(75,60);
-    T.forward(75,240);
-    T.forward(75,60);
-    T.forward(75,240);
-    T.forward(75,60);
-
-    
-    /*T.penup();
-    T.forward(210);
-    T.pendow();
-    T.forward(50,90);
-    T.move(250,250);
-    T.forward(100);*/
+    T.forward(150);
     glutMainLoop();
 }
